0f33ce20ba27b9de4812fa764b0acbae65360db3
Dani Vijay <danimvijay@gmail.com>
2018-01-14 15:38:34 +0530

    reduce1, arrow fns tested on node OK

--- reduse1.js
+++ reduse1.js
@@ -1,13 +1,17 @@
+'use strict'
+
 var values = [
 		{ amt: 100 },
 		{ amt: 100 },
 		{ amt: 100 },
 		{ amt: 100 }
 	];
-
+	
 var sum1 = values.reduce(function(sum, value){
 	return sum + value.amt;
 }, 0);
 
 //Something went wrong here!!
-//var sum2 = values.reduce((sum, value) => sum + value.amt, 0);
+var sum2 = values.reduce((sum, value) => sum + value.amt, 0);
+
+console.log(sum1, sum2);
